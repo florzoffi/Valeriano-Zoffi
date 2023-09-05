c0be8b5b41f243b41ceb7724ecacd604c4a1c565
Florencia Zoffi <136618657+florzoffi@users.noreply.github.com>
2023-09-05 01:53:16 -0300

    init commit

---
+++ Queue.java
@@ -0,0 +1,49 @@
+package queue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue<T> {
+    private List<T> elements;
+
+    public static String QueueEmpty = "Queue is empty";
+
+    public Queue() {
+        elements = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public Queue<T> add(T cargo) {
+        if (elements.contains(cargo)) {
+            throw new RuntimeException("Duplicate element not allowed");
+        }
+
+        elements.add(cargo);
+        return this;
+    }
+
+    public T take() {
+        if (isEmpty()) {
+            throw new RuntimeException(QueueEmpty);
+        }
+
+        T cargo = elements.remove(0);
+        return cargo;
+    }
+
+    public T head() {
+        try {
+            return elements.get(0);
+        } catch (IndexOutOfBoundsException e) {
+            throw new RuntimeException(QueueEmpty);
+        }
+    }
+
+
+    public int size() {
+        return elements.size();
+    }
+}
