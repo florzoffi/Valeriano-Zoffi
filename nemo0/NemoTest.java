b0dea63b948c05eb3f7976ef1770edbae6baba0a
Isivaleriano <126919136+Isivaleriano@users.noreply.github.com>
2023-10-21 15:29:02 -0300

    Update NemoTest.java

--- nemo0/NemoTest.java
+++ nemo0/NemoTest.java
@@ -17,7 +17,7 @@ public class NemoTest<E> {
 		assertEquals( new ArrayList<>(Arrays.asList(0, 0)) , nemo().getPosition());
 	}
 	
-	@Test public void test03NemoDirection() {
+	@Test public void test03NemoDirectionStartsOnRight() {
 		assertEquals("right", nemo().getDirection());
 	}
 	
@@ -33,7 +33,7 @@ public class NemoTest<E> {
         assertEquals(0, nemoDidThis("u").getDepth());
 	}
 	
-	@Test public void test07NemoRotates90DegreesToTheLeft() {
+	@Test public void test07NemoRotates90DegreesToTheLeft() {	
         assertEquals("up", nemoDidThis("l").getDirection());
 	}
 	
@@ -41,46 +41,79 @@ public class NemoTest<E> {
         assertEquals("down", nemoDidThis("r").getDirection());
 	}
 	
-	@Test public void test09NemoGoesForward() {
+	@Test public void test09NemoRotates180DegreesToTheRight() {
+        assertEquals("left", nemoDidThis("rr").getDirection());
+	}
+	
+	@Test public void test10NemoGoesForward() {
         assertEquals(new ArrayList<>(Arrays.asList(1, 0)), nemoDidThis("f").getPosition());
 	}
 	
-	@Test public void test10NemoDoesSeveralIndicationsAtTheSameTimeInSurface() {
-		nemosStateVerification(nemoDidThis("flffrrf"), 0, new ArrayList<>(Arrays.asList(1, 1)), "down", Nemo.noCapsuleThrown );
+	
+	@Test public void test11NemoRotatesRightTwiceWhileGoingForward() {
+		assertEquals(new ArrayList<>(Arrays.asList(0, -1)), nemoDidThis("frfrf").getPosition());
 	}
 	
-	@Test public void test11NemoDoesSeveralIndicationsAtTheSameTime() {
-		nemosStateVerification(nemoDidThis("flfdddufrrf"), 2, new ArrayList<>(Arrays.asList(1, 1)), "down", Nemo.noCapsuleThrown);
+	@Test public void test12NemoRotatesLeftTwiceWhileGoingForward() {
+		assertEquals(new ArrayList<>(Arrays.asList(0, 1)), nemoDidThis("flflf").getPosition());
 	}
 	
-	@Test public void test12NemoThrowsCapsule() {
+	@Test public void test13NemoRotates360DegreesToTheLeft() {
+		assertEquals("right", nemoDidThis("llll").getDirection());	
+	}
+	
+	@Test public void test14NemoMovesOnACircleAndGoesBackToOriginalePosition() {
+		Nemo nemo = nemo();
+		assertEquals(new ArrayList<>(Arrays.asList(0, 0)), nemo.getPosition());
+		nemo.action("frfrfrf");
+		assertEquals(new ArrayList<>(Arrays.asList(0, 0)), nemo.getPosition());
+	}
+	
+	@Test public void test15NemoThrowsCapsule() {
+		assertEquals( Nemo.successfullLaunch, nemoDidThis("m").getMessage() );
+	}
+	
+	@Test public void test16NemoThrowsCapsuleInDepth1() {
 		assertEquals( Nemo.successfullLaunch, nemoDidThis("dm").getMessage() );
 	}
-		
-	@Test public void test13NemoCantThrowsCapsuleUnderDeapth1() {
-		assertEquals( Nemo.submarineHasExploded , assertThrows(Error.class, ()-> nemoDidThis("ddm")).getMessage() );
+	
+	@Test public void test17NemoCantThrowsCapsuleUnderDeapth1() {
+		assertEquals( Nemo.submarineHasExploded , assertThrows(Exception.class, ()-> nemoDidThis("ddm")).getMessage() );
 	}
 	
-	@Test public void test15NemoCanKeepGoingAfterThrowingCapsuleRight() {
-		assertEquals( Nemo.successfullLaunch, nemoDidThis("dmfrfudum").getMessage());
+	@Test public void test18NemoGoesDownThreeTimesAndUpTwoAndThrowsCapsule(){
+		assertEquals( Nemo.successfullLaunch, nemoDidThis("ddduum").getMessage() );
+	}
+	
+	@Test public void test19NemoGoesDownFourTimesAndUpOneAndCantThrowsCapsule(){
+		assertEquals( Nemo.submarineHasExploded , assertThrows(Exception.class, ()-> nemoDidThis("ddddum")).getMessage() );
+	}
+	
+	@Test public void test20NemoCanMoveUnderSurface(){
+		nemosStateVerification(nemoDidThis("dddfrfrf"), 3, new ArrayList<>(Arrays.asList(0, -1)), "left", Nemo.noCapsuleThrown);	
+	}
+	
+	@Test public void test21NemoCanMoveAndThrowCapsulesInDepth1() {
+		nemosStateVerification(nemoDidThis("dmfrfrf"), 1, new ArrayList<>(Arrays.asList(0, -1)), "left", Nemo.successfullLaunch);
+	}
+	
+	@Test public void test22NemoCanThrowCapsulesMoreThanOneTime() {
+		assertEquals( 4 , nemoDidThis("mmmm").thrownCapsules );
+	}
+	
+	@Test public void test23NemoDoesSeveralIndicationsAtTheSameTimeInSurface() {
+		nemosStateVerification(nemoDidThis("flffrrfm"), 0, new ArrayList<>(Arrays.asList(1, 1)), "down", Nemo.successfullLaunch);
+	}
+	
+	@Test public void test24NemoDoesSeveralIndicationsAtTheSameTimeOnDepth() {
+		nemosStateVerification(nemoDidThis("ddduflffrrf"), 2, new ArrayList<>(Arrays.asList(1, 1)), "down", Nemo.noCapsuleThrown);
+	}
+	
+	@Test public void test25NemoDoesSeveralIndicationsBothAtOnDepthAndSurface() {
+		nemosStateVerification(nemoDidThis("flffddduurrfm"), 1, new ArrayList<>(Arrays.asList(1, 1)), "down", Nemo.successfullLaunch);
 	}
 
-	@Test
-	public void test17NemoIgnoresUnknownCommands() {
-	    nemosStateVerification(nemoDidThis("XhpO"), 0, new ArrayList<>(Arrays.asList(0, 0)), "right", Nemo.noCapsuleThrown );
-	
-	}
-	@Test
-	public void test18NemoIgnoresUnknownCommandsButDoesFollowsKnownOnes() {
-	    nemosStateVerification(nemoDidThis("XdXdXXffXXlfXXumX"), 1, new ArrayList<>(Arrays.asList(2, 1)), "up", Nemo.successfullLaunch);
-	}
-	
-	@Test
-	public void test19NemoIgnoresEmptySpaces() {
-	    nemosStateVerification(nemoDidThis(" d d  ff  lf  um"), 1, new ArrayList<>(Arrays.asList(2, 1)), "up", Nemo.successfullLaunch);
-	}
-	
-	
+
 	private Nemo nemo() {
 		Nemo nemo = new Nemo();
 		return nemo;
