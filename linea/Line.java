bf2f70e961e1facb8992e601df203be61c6b2a51
Isivaleriano <126919136+Isivaleriano@users.noreply.github.com>
2023-11-06 23:22:54 -0300

    Update Line.java

--- linea/Line.java
+++ linea/Line.java
@@ -36,9 +36,61 @@ public class Line {
         return this;
 	}
 	
+	
 	public char[] show() {
-		// TODO Auto-generated method stub
-		return null;
+	    char[] displayBoard = new char[(base + 3) * (height + 2)];
+
+	    for (int col = 0; col < base + 2; col++) {
+	        for (int row = 0; row < height + 2; row++) {
+	            if (col == 0 || col == base + 1) {
+	                // Bordes verticales de las columnas
+	                displayBoard[row * (base + 3) + col] = '|';
+	            } else if (row == 0 || row == height + 1) {
+	                // Borde horizontal superior e inferior
+	                displayBoard[row * (base + 3) + col] = '-';
+	            } else {
+	                // Celdas del juego
+	                char cellValue = gameBoard.grid.get(col - 1).get(row - 1).getPlayerColor();
+	                displayBoard[row * (base + 3) + col] = cellValue;
+	            }
+
+	            if (col == base + 1) {
+	                // Salto de línea después de la última columna
+	                displayBoard[row * (base + 3) + col + 1] = '\n';
+	            }
+	        }
+	    }
+
+	    return displayBoard;
+	}
+	
+	public Line playBlueAt(int column) {
+	    if (gameStatus.isGameFinished() || !player.isTurn("Blue")) {
+	        throw new RuntimeException("It's not the Blue player's turn.");
+	    }
+
+	    if (column < 0 || column >= gameBoard.getNumColumns() || gameBoard.isColumnFull(column)) {
+	        throw new RuntimeException("Invalid move. Your turn has been lost");
+	    }
+
+	    gameBoard.placeChip(column, 'B');
+	    player = new RedTurn(); // Cambiamos el turno al jugador rojo
+	    return this;
+	}
+
+
+	public Line playRedAt(int column) {
+	    if (gameStatus.isGameFinished() || !player.isTurn("Red")) {
+	        throw new RuntimeException("It's not the Red player's turn.");
+	    }
+
+	    if (column < 0 || column >= gameBoard.getNumColumns() || gameBoard.isColumnFull(column)) {
+	        throw new RuntimeException("Invalid move. Your turn has been lost");
+	    }
+
+	    gameBoard.placeChip(column, 'R');
+	    player = new BlueTurn(); // Cambiamos el turno al jugador azul
+	    return this;
 	}
 	
 	public boolean redTurn() {
