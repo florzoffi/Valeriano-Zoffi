4043b5877ec8d55a36d590d1cf84561389d93798
Isivaleriano <126919136+Isivaleriano@users.noreply.github.com>
2023-11-06 23:21:59 -0300

    Update Game.java

--- linea/Game.java
+++ linea/Game.java
@@ -1,21 +1,36 @@
 package linea;
 
-public class Game {
+import java.util.Scanner;
 
-    public static void main( String[] args) throws Exception {
-        System.out.println( "Dimensions?");
-        Line game = new Line( prompt( "Base? " ), prompt( "Height? " ), 'C' );
-        System.out.println( game.show() );
-    
-      while ( !game.finished() ) {
-		game.play( null, prompt( "Where? " ) );
-          System.out.println( game.show() );
-      }
-    
+public class Game {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Dimensiones?");
+        Scanner scanner = new Scanner(System.in);
+        int base = promptAsInt(scanner, "Base? ");
+        int altura = promptAsInt(scanner, "Altura? ");
+        char estrategia = promptAsChar(scanner, "Estrategia de Juego: A, B o C? ");
+
+        Line game = new Line(base, altura, estrategia);
+        System.out.println(game.show());
+
+        while (!game.finished()) {
+            game.playRedAt(promptAsInt(scanner, "Rojas? "));
+            System.out.println(game.show());
+
+            if (!game.finished()) {
+                game.playBlueAt(promptAsInt(scanner, "Azul? "));
+                System.out.println(game.show());
+            }
+        }
     }
 
-    private static int prompt( String message ) {
-        System.out.print( message );
-        return Integer.parseInt( System.console().readLine() );
+    private static int promptAsInt(Scanner scanner, String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    private static char promptAsChar(Scanner scanner, String message) {
+        System.out.print(message);
+        return scanner.next().charAt(0);
     }
 }
