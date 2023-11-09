ac080a41681087166073661df7e599c3edc9de32
Isivaleriano <126919136+Isivaleriano@users.noreply.github.com>
2023-11-09 00:13:24 -0300

    Update Game.java

--- linea/Game.java
+++ linea/Game.java
@@ -1,36 +1,33 @@
 package linea;
 
-import java.util.Scanner;
-
 public class Game {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Dimensiones?");
-        Scanner scanner = new Scanner(System.in);
-        int base = promptAsInt(scanner, "Base? ");
-        int altura = promptAsInt(scanner, "Altura? ");
-        char estrategia = promptAsChar(scanner, "Estrategia de Juego: A, B o C? ");
+	  public static void main( String[] args) throws Exception {
+	    System.out.println( "Dimensiones?");
+	    Line game = new Line( promptAsInt( "Base? " ), 
+	                            promptAsInt( "Altura? " ), 
+	                            promptAsChar( "Estartegia de Juego: A, B o C? " ) );
+	    
+	    System.out.println( game.show() );
+	    
+	    while ( !game.finished() ) {
+	      game.playRedAt( promptAsInt( "Rojas? " ) );
+	      System.out.println( game.show() );
+	      
+	      if ( !game.finished() ) {
+	        game.playBlueAt( promptAsInt( "Azul? " ) );
+	        System.out.println( game.show() );
+	      }
+	    }
+	    
+	  }
 
-        Line game = new Line(base, altura, estrategia);
-        System.out.println(game.show());
-
-        while (!game.finished()) {
-            game.playRedAt(promptAsInt(scanner, "Rojas? "));
-            System.out.println(game.show());
-
-            if (!game.finished()) {
-                game.playBlueAt(promptAsInt(scanner, "Azul? "));
-                System.out.println(game.show());
-            }
-        }
-    }
-
-    private static int promptAsInt(Scanner scanner, String message) {
-        System.out.print(message);
-        return scanner.nextInt();
-    }
-
-    private static char promptAsChar(Scanner scanner, String message) {
-        System.out.print(message);
-        return scanner.next().charAt(0);
-    }
-}
+	  private static int promptAsInt( String message ) {
+	    System.out.print( message );
+	    return Integer.parseInt( System.console().readLine() );
+	  }
+	  
+	  private static char promptAsChar( String message ) {
+	    System.out.print( message );
+	    return System.console().readLine().charAt( 0 );
+	  }
+	}
